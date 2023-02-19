cf6bc34d21bfae10f9dffd25afccd43ef83ea62b
Maryam Sharomi <sharomimaryamolabisi@gmail.com>
2023-02-19 04:22:09 -0800

    still fixing 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-int main (void)
+
+/**
+ * main -  prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: 0
+ */
+
+int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
